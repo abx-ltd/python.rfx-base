55ed4978b76a153f916921f1abd75f806409a16d
Phu Nguyen <phunguyen@Phus-MacBook-Pro.local>
2025-06-09 11:59:34 +0700

    update: test policy

--- tests/rfx_user/test_policy.py
+++ tests/rfx_user/test_policy.py
@@ -16,7 +16,8 @@ def domain():
 
 
 @pytest.mark.asyncio
-async def test_command_policy(domain):
+async def test_command_policy_profile(domain):
+	usr = ""
 	sub = FIXTURE_PROFILE_ID
 	org = FIXTURE_ORGANIZATION_ID
 	dom = "user-profile"
@@ -47,6 +48,56 @@ async def test_command_policy(domain):
 	]
 	for act, res, rid, expect in testcases:
 		prev = time.time()
-		rs = await domain.policymgr.check(sub, org, dom, res, rid, act)
+		rs = await domain.policymgr.check(usr, sub, org, dom, res, rid, act)
+		logger.info("[%ss] Result %s" % (time.time() - prev, rs))
+		assert rs.allowed == expect
+
+@pytest.mark.asyncio
+async def test_command_policy_user_access(domain):
+	usr = "88212396-02c5-46ae-a2ad-f3b7eb7579c0"
+	sub = ""
+	org = ""
+	dom = "user-profile"
+	act = None
+	res = None
+	rid = None
+
+	testcases = [
+		# User role: self-access
+		("update-user", "user", usr, True),
+		("send-action", "user", usr, False),
+		("send-verification", "user", usr, False),
+		("deactivate-user", "user", usr, False),
+		("activate-user", "user", usr, False),
+		("sync-user", "user", usr, False),
+	]
+	for act, res, rid, expect in testcases:
+		prev = time.time()
+		rs = await domain.policymgr.check(usr, sub, org, dom, res, rid, act)
+		logger.info("[%ss] Result %s" % (time.time() - prev, rs))
+		assert rs.allowed == expect
+
+@pytest.mark.asyncio
+async def test_command_policy_user_admin(domain):
+	usr = "44d2f8cb-0d46-4323-95b9-c5b4bdbf6205"
+	sub = ""
+	org = ""
+	dom = "user-profile"
+	act = None
+	res = None
+	rid = None
+
+	testcases = [
+		# User role: sys-admin
+		("send-action", "user", usr, True),
+		("update-user", "user", usr, True),
+		("send-verification", "user", usr, True),
+		("deactivate-user", "user", usr, True),
+		("activate-user", "user", usr, True),
+		("sync-user", "user", usr, True),
+	]
+	for act, res, rid, expect in testcases:
+		prev = time.time()
+		rs = await domain.policymgr.check(usr, sub, org, dom, res, rid, act)
 		logger.info("[%ss] Result %s" % (time.time() - prev, rs))
 		assert rs.allowed == expect
