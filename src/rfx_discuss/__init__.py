da3bb6a4a753a29fb8302a09c1b5b33889327a8f
Hung X. Le <lexhung@gmail.com>
2025-10-17 17:06:38 +0700

    add: notes

--- src/rfx_discuss/__init__.py
+++ src/rfx_discuss/__init__.py
@@ -1,2 +1,5 @@
 # A discussion attached to an aggroot without needing to be aware what is the aggroot is about
 # This allow the discussion to be attached to any item.
+# See:
+#    - https://areknawo.com/top-6-disqus-alternatives-for-technical-blogging/
+#    - https://demo.commento.io/#commento-login-box-container
