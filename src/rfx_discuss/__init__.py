0ff3d44a90d47c3fb2c2ba02ee968802827205c0
dinhlooc <locnd@adaptive-bits.com>
2025-10-21 14:53:40 +0700

    build sourse base for discuss domain

--- src/rfx_discuss/__init__.py
+++ src/rfx_discuss/__init__.py
@@ -1,5 +1,15 @@
-# A discussion attached to an aggroot without needing to be aware what is the aggroot is about
-# This allow the discussion to be attached to any item.
+# A Discuss attached to an aggroot without needing to be aware what is the aggroot is about
+# This allow the Discuss to be attached to any item.
 # See:
 #    - https://areknawo.com/top-6-disqus-alternatives-for-technical-blogging/
 #    - https://demo.commento.io/#commento-login-box-container
+
+
+""" RFX Discuss """
+
+from ._meta import config, logger
+from .domain import RFXDiscussDomain
+from .query import RFXDiscussQueryManager
+from .provider import RFXDiscussProfileProvider
+from . import command
+
