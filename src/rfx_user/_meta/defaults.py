57e26659292c33482fd33b8c139a823921647c55
dinhlooc <locnd@adaptive-bits.com>
2025-10-24 14:12:15 +0700

    fix user_profile_schema

--- src/rfx_user/_meta/defaults.py
+++ src/rfx_user/_meta/defaults.py
@@ -3,6 +3,6 @@ from rfx_base import config
 LOG_LEVEL = config.LOG_LEVEL
 DB_DSN = config.DB_DSN
 
-USER_PROFILE_SCHEMA = "rfx-user"
+USER_PROFILE_SCHEMA = "cpo-user"
 POLICY_SCHEMA = "rfx-policy"
 POLICY_TABLE = "_policy--user-profile"
