ea4372819a91280030afa8e234a2ce3558e7953b
dinhlooc <locnd@adaptive-bits.com>
2025-10-28 15:37:43 +0700

    update: config defaults

--- src/rfx_message/_meta/defaults.py
+++ src/rfx_message/_meta/defaults.py
@@ -3,7 +3,7 @@ from rfx_base import config
 LOG_LEVEL = config.LOG_LEVEL
 DB_DSN = config.DB_DSN
 
-MESSAGE_SERVICE_SCHEMA = config.RFX_MESSAGE_SCHEMA
-POLICY_SCHEMA = config.RFX_POLICY_SCHEMA
+MESSAGE_SERVICE_SCHEMA = "rfx_message"
+POLICY_SCHEMA = "rfx_policy"
 POLICY_TABLE = "_policy_rfx_message"
 NAMESPACE = "rfx-message"
