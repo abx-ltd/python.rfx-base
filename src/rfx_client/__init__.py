97584dc8457dd9db564eacbb004c1079721c5696
dinhlooc-abx <locnd@adaptive-bits.com>
2025-11-04 17:27:29 +0700

    remove file message

--- src/rfx_client/__init__.py
+++ src/rfx_client/__init__.py
@@ -4,5 +4,5 @@ from ._meta import config, logger
 from .domain import RFXClientDomain
 from .query import RFXClientQueryManager
 from .provider import RFXClientProfileProvider
-from . import message, command, domain, datadef, query
+from . import command, domain, datadef, query
 from . import command
