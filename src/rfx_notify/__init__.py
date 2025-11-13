acc7e60fc6221daaf1451a0d14178606f3eeb41d
@ABX Jay Nguyen <vunna@adaptive-bits.com>
2025-11-13 17:23:41 +0700

    fix: boostrap worker

--- src/rfx_notify/__init__.py
+++ src/rfx_notify/__init__.py
@@ -3,5 +3,4 @@ from ._meta import config, logger
 
 from .domain import NotifyServiceDomain
 from .query import NotifyServiceQueryManager
-from .bootstrap import bootstrap_providers
 from . import command
