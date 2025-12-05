aa193d143cc5bdc790be4dc2e89ae1058b791ee1
@ABX Jay Nguyen <vunna@adaptive-bits.com>
2025-12-05 16:57:51 +0700

    update: policy

--- src/rfx_schema/rfx_policy/_pgentity.py
+++ src/rfx_schema/rfx_policy/_pgentity.py
@@ -24,7 +24,7 @@ SELECT uuid_generate_v4() AS _id,
     NULL::timestamp without time zone AS _deleted
    FROM {config.RFX_POLICY_SCHEMA}.policy_role pol_rol
      JOIN {config.RFX_POLICY_SCHEMA}.policy_resource pol_res ON pol_rol.policy_key::text = pol_res.policy_key::text
-  WHERE pol_res.domain::text = 'user-profile'::text
+  WHERE pol_res.domain::text = 'user-profile'::text ANY (ARRAY['user-profile'::text, 'x'::text])
 UNION ALL
  SELECT uuid_generate_v4() AS _id,
     'g'::character varying(255) AS ptype,
