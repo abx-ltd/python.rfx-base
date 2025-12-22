4234dca8efad2e7a0a67379bb78ecdf36769b54b
@ABX Jay Nguyen <vunna@adaptive-bits.com>
2025-12-22 16:05:27 +0700

    update: query

--- src/rfx_user/query.py
+++ src/rfx_user/query.py
@@ -124,22 +124,23 @@ async def reject_invitation(query_manager: UserProfileQueryManager, request: Req
 async def switch_profile(query_manager: UserProfileQueryManager, request: Request, profile_id: str):
     context = request.state.auth_context
     profile_id = profile_id
-    profile = await query_manager.data_manager.fetch('profile', profile_id)
-    if profile.user_id != context.profile.user_id:
-        return {"error": "Profile does not belong to the current user"}
-    profiles = await query_manager.find_all(
-        'profile',
-        where=dict(
-            user_id=context.profile.user_id,
-            status='ACTIVE',
-            _realm=config.REALM
-        ))
+    async with query_manager.data_manager.transaction():
+        profile = await query_manager.data_manager.fetch('profile', profile_id)
+        if profile.user_id != context.profile.usr_id:
+            return {"error": "Profile does not belong to the current user"}
+        profiles = await query_manager.find_all(
+            'profile',
+            where=dict(
+                user_id=context.profile.usr_id,
+                status='ACTIVE',
+                _realm=config.REALM
+            ))
 
-    for p in profiles:
-        if p._id == profile._id:
-            await query_manager.data_manager.update(p, current_profile=True)
-        else:
-            await query_manager.data_manager.update(p, current_profile=False)
+        for p in profiles:
+            if p._id == profile._id:
+                await query_manager.data_manager.update(p, current_profile=True)
+            else:
+                await query_manager.data_manager.update(p, current_profile=False)
 
     redirect_url = config.REALM_URL_MAPPER.get(config.REALM, '/')
     return RedirectResponse(redirect_url, status_code=302)
