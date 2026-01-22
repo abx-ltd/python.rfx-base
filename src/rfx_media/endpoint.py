8b3f7abddf0457e0df8691e2b072923679bd1928
@ABX Jay Nguyen <vunna@adaptive-bits.com>
2026-01-22 11:53:40 +0700

    update: idm

--- src/rfx_media/endpoint.py
+++ src/rfx_media/endpoint.py
@@ -1,72 +1,72 @@
-# import json
-# from typing import Annotated
-# from fastapi import Depends, Form, UploadFile, Request
-# from fastapi.responses import StreamingResponse
-# from fluvius.media import MediaInterface, FsSpecCompressionMethod
-# from fluvius.fastapi.auth import auth_required
-# from pipe import Pipe
-# from pydantic import BaseModel
+import json
+from typing import Annotated
+from fastapi import Depends, Form, UploadFile, Request
+from fastapi.responses import StreamingResponse
+from fluvius.media import MediaInterface, FsSpecCompressionMethod
+from fluvius.fastapi.auth import auth_required
+from pipe import Pipe
+from pydantic import BaseModel
 
-# class MediaMetadata(BaseModel):
-#     fs_key: str = None
-#     compress: FsSpecCompressionMethod = None
-#     resource: str
-#     resource_id: str
-
-
-# def parse_form(metadata: Annotated[str, Form()]):
-#     return MediaMetadata(**json.loads(metadata))
+class MediaMetadata(BaseModel):
+    fs_key: str = None
+    compress: FsSpecCompressionMethod = None
+    resource: str
+    resource_id: str
 
 
-# @Pipe
-# def configure_media(app):
-#     if hasattr(app.state, 'media'):
-#         return app
+def parse_form(metadata: Annotated[str, Form()]):
+    return MediaMetadata(**json.loads(metadata))
 
-#     app.state.media = MediaInterface(app)
 
-#     @app.post("/media:upload")
-#     @auth_required()
-#     async def upload_files(
-#         request: Request,
-#         file: UploadFile,
-#         metadata: Annotated[MediaMetadata, Depends(parse_form)],
-#     ):
-#         media = app.state.media
-#         entry = await media.put(
-#             file.file,
-#             filename=file.filename,
-#             mime_type=file.content_type,
-#             **metadata.model_dump()
-#         )
-#         return entry
+@Pipe
+def configure_media(app):
+    if hasattr(app.state, 'media'):
+        return app
 
-#     @app.get("/media/{file_id}")
-#     @auth_required()
-#     async def get_file(
-#         request: Request,
-#         file_id: str
-#     ):
-#         media = app.state.media
-#         metadata = await media.get_metadata(file_id)
-#         return StreamingResponse(
-#             await media.stream(file_id),
-#             media_type=metadata.filemime,
-#         )
+    app.state.media = MediaInterface(app)
 
-#     @app.get("/media/{file_id}/download")
-#     @auth_required()
-#     async def download_file(
-#         request: Request,
-#         file_id: str
-#     ):
-#         media = app.state.media
-#         metadata = await media.get_metadata(file_id)
-#         return StreamingResponse(
-#             await media.stream(file_id),
-#             media_type=metadata.filemime,
-#             headers={
-#                 "Content-Disposition": f"attachment; filename={metadata.filename}"
-#             })
+    @app.post("/media:upload")
+    @auth_required()
+    async def upload_files(
+        request: Request,
+        file: UploadFile,
+        metadata: Annotated[MediaMetadata, Depends(parse_form)],
+    ):
+        media = app.state.media
+        entry = await media.put(
+            file.file,
+            filename=file.filename,
+            mime_type=file.content_type,
+            **metadata.model_dump()
+        )
+        return entry
 
-#     return app
+    @app.get("/media/{file_id}")
+    @auth_required()
+    async def get_file(
+        request: Request,
+        file_id: str
+    ):
+        media = app.state.media
+        metadata = await media.get_metadata(file_id)
+        return StreamingResponse(
+            await media.stream(file_id),
+            media_type=metadata.filemime,
+        )
+
+    @app.get("/media/{file_id}/download")
+    @auth_required()
+    async def download_file(
+        request: Request,
+        file_id: str
+    ):
+        media = app.state.media
+        metadata = await media.get_metadata(file_id)
+        return StreamingResponse(
+            await media.stream(file_id),
+            media_type=metadata.filemime,
+            headers={
+                "Content-Disposition": f"attachment; filename={metadata.filename}"
+            })
+
+    return app
