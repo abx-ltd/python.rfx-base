0ad8459efb1596ecc1361c5c760f8a80232424c6
Hung X. Le <lexhung@gmail.com>
2026-04-10 01:23:53 +0700

    Remove legacy rfx_discussion; drop duplicate notify mdcontext; add rfx_setting stub
    - Delete legacy/rfx_discussion package (cleanup)
    - Remove src/rfx_notify/mdcontext markdown copies; docs live under docs/rfx_notify
    - Add empty src/rfx_setting package placeholder

--- legacy/rfx_discussion/__init__.py
+++
@@ -1,8 +0,0 @@
-""" RFX Discussion """
-
-from ._meta import config, logger
-from .domain import RFXDiscussionDomain
-from .query import RFXDiscussionQueryManager
-from .provider import RFXDiscussionProfileProvider
-from . import command
-from . import message
--- legacy/rfx_discussion/_meta/__init__.py
+++
@@ -1,4 +0,0 @@
-from fluvius import setupModule
-from . import defaults
-
-config, logger = setupModule(__name__, defaults)
