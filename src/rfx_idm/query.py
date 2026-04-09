6a5e33da07894b503c3ca5b25f0e898c5a60d7d1
@ABX Van Then <thangnv@adaptive-bits.com>
2026-04-09 13:27:06 +0700

    update check email

--- src/rfx_idm/query.py
+++ src/rfx_idm/query.py
@@ -260,7 +260,7 @@ async def check_user_email(
         {"exists": true|false}
     """
     async with query_manager.data_manager.transaction():
-        email = request.query_params.get("email")
+        email = request.query_params.get("email").strip().lower()
         if not email:
             return {"error": "email query parameter is required"}
 
