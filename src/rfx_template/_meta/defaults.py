a9d0c140bd1bfe2e10b984add0288f2bfde4a155
Duc Thanh <thanhld@adaptive-bits.com>
2026-04-15 15:20:38 +0700

    update 2d mess resolve conflict

--- src/rfx_template/_meta/defaults.py
+++ src/rfx_template/_meta/defaults.py
@@ -1,7 +1,8 @@
 from rfx_base import config as base_config
+from rfx_schema import config as schema_config
 
 LOG_LEVEL = base_config.LOG_LEVEL
-DB_DSN = base_config.RFX_TEMPLATE_DB_DSN
+DB_DSN = schema_config.RFX_TEMPLATE_DB_DSN
 
 RFX_TEMPLATE_SCHEMA = "rfx_template"
 NAMESPACE = "rfx-template"
